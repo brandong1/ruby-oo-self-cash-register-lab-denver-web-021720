99a93a2d346857259cf90e046d2c352a29a06a8e
github username <github email address>
2020-02-22 17:34:23 -0700

    create add_item method and pass test

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,21 @@
 class CashRegister
-attr_accessor :total, :discount
+attr_accessor :total, :discount, :items
 
     def initialize(discount = 0)
       @total = 0
       @discount = discount
       @items = []
     end
+
+    def add_item(title, amount, item_quantity=1)
+        self.total += amount * item_quantity
+        item_quantity.times do 
+            items << title
+        end
+    end
+
+    def apply_discount
+        
+    end
+
 end
